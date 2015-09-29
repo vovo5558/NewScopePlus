570235d4f2aa555da0f50713948160a9aa4c6876
vovo5558 <vovo5558@gmail.com>
2015-09-29 13:54:04 +0800

    upload version 1

---
+++ ScopePlus/Assets/Scripts/ZoomManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ZoomManager : MonoBehaviour {
+
+	public Transform TranslateLeft;
+	public Transform TranslateRight;
+	public Transform ScaleLeft;
+	public Transform ScaleRight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	//public float CurrentZoom;
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKey(KeyCode.Z)) 
+		{
+			ScaleLeft.localScale += new Vector3(0.04f, 0.04f, 0.04f);
+			ScaleRight.localScale += new Vector3(0.04f, 0.04f, 0.04f);
+			//leftCamera
+		}
+
+		if (Input.GetKey(KeyCode.X)) 
+		{
+			ScaleLeft.localScale -= new Vector3(0.04f, 0.04f, 0.04f);
+			ScaleRight.localScale -= new Vector3(0.04f, 0.04f, 0.04f);
+			//leftCamera
+		}
+
+		if (Input.GetKey (KeyCode.C)) 
+		{
+			TranslateLeft.localPosition += new Vector3(0.04f, 0.0f, 0.0f);
+			TranslateRight.localPosition -= new Vector3(0.04f, 0.0f, 0.0f);
+			//leftCamera
+		}
+		
+		if (Input.GetKey (KeyCode.V)) 
+		{
+			TranslateLeft.localPosition -= new Vector3(0.04f, 0.0f, 0.0f);
+			TranslateRight.localPosition += new Vector3(0.04f, 0.0f, 0.0f);
+			//leftCamera
+		}
+	}
+}
